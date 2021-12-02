f0dc7a6cae0fc751b0041ebf4bdf5a248cdf3824
Glen Menezes <glenmenezes@Glens-MacBook-Air.local>
2021-12-01 21:00:05 -0800

    add solution for second Day 1 problem

--- Day1SonarSweep.scala
+++ Day1SonarSweep.scala
@@ -2,19 +2,50 @@ import scala.util.Using
 import scala.io.Source
 
 object Day1SonarSweep extends App {
-  Using(Source.fromFile("./inputs/day-1-input.txt")) { source =>
-    val allDepths: List[Int] = source.getLines.map(_.toInt).toList
+  val allDepths: List[Int] =
+    Using(Source.fromFile("./inputs/day-1-input.txt")) {
+      _.getLines.map(_.toInt).toList
+    }.get
 
+  def numTimesMeasurementIncreases: Int = {
     def loop(depths: List[Int], count: Int): Int =
       depths match {
         // Terminal case
         case Nil => count
+
         // The depth increased, increase counter
         case x :: y :: zs if x < y => loop(y :: zs, count + 1)
+
         // The depth did not increase or its the last entry in the input
         case x :: ys => loop(ys, count)
       }
 
-    println(loop(allDepths, 0))
+    loop(allDepths, 0)
   }
+
+  def numTimesMeasurementSlidingWindowIncreases: Int = {
+    def loop(depths: List[Int], count: Int): Int =
+      depths match {
+        // Atleast 4 elements need to exist for a comparison
+        // two sliding widows are : (a, b, c) & (b, c, d)
+        // the difference in sum between the second and first windows = d - a
+        // if this difference is positive, increase counter
+        case a :: b :: c :: d :: es if d > a =>
+          loop(b :: c :: d :: es, count + 1)
+
+        // if the difference is not positive, no change to counter
+        case a :: b :: c :: d :: es => loop(b :: c :: d :: es, count)
+
+        // Terminal case: There are less than 3 elements
+        case _ => count
+      }
+
+    loop(allDepths, 0)
+  }
+
+  // solution to part 1
+  println(numTimesMeasurementIncreases)
+
+  // solution to part 2
+  println(numTimesMeasurementSlidingWindowIncreases)
 }
