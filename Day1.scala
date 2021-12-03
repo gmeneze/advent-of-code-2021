41a8f5f40e17495cc79ccb19b4dbfe17565106fa
Glen Menezes <gmeneze@ncsu.edu>
2021-12-02 18:36:21 -0800

    make Day1 solution more functional

--- Day1.scala
+++ Day1.scala
@@ -2,39 +2,20 @@ import InputReader._
 
 object Day1 extends App {
   def solutionToFirstHalf(allDepths: List[Int]): Int = {
-    def loop(depths: List[Int], count: Int): Int =
-      depths match {
-        // Terminal case
-        case Nil => count
-
-        // The depth increased, increase counter
-        case x :: y :: zs if x < y => loop(y :: zs, count + 1)
-
-        // The depth did not increase or its the last entry in the input
-        case x :: ys => loop(ys, count)
-      }
-
-    loop(allDepths, 0)
+    val slidingWindowOfTwoElems: Iterator[List[Int]] = allDepths.sliding(2, 1)
+    slidingWindowOfTwoElems.foldLeft(0) {
+      case (acc, List(firstDepth, secondDepth)) =>
+        (secondDepth > firstDepth) match {
+          case true  => acc + 1
+          case false => acc
+        }
+    }
   }
 
   def solutionToSecondHalf(allDepths: List[Int]): Int = {
-    def loop(depths: List[Int], count: Int): Int =
-      depths match {
-        // Atleast 4 elements need to exist for a comparison
-        // two sliding widows are : (a, b, c) & (b, c, d)
-        // the difference in sum between the second and first windows = d - a
-        // if this difference is positive, increase counter
-        case a :: b :: c :: d :: es if d > a =>
-          loop(b :: c :: d :: es, count + 1)
-
-        // if the difference is not positive, no change to counter
-        case a :: b :: c :: d :: es => loop(b :: c :: d :: es, count)
-
-        // Terminal case: There are less than 4 elements
-        case _ => count
-      }
-
-    loop(allDepths, 0)
+    val sumOfSlidingWindows: List[Int] =
+      allDepths.sliding(3, 1).map(_.sum).toList
+    solutionToFirstHalf(sumOfSlidingWindows)
   }
 
   val allDepths: List[Int] = readAllLines("day-1-input.txt")
